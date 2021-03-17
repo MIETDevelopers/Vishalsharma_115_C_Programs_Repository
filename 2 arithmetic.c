c065c47e71ff3abd4e65a2d53cd256b64de7a828
vishal561 <76278080+vishal561@users.noreply.github.com>
2021-03-16 23:19:12 -0700

    Add files via upload

---
+++ 2 arithmetic.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+int main()
+{
+   int first, second, add, subtract, multiply;
+   
+ 
+   printf("Enter two integers\n");
+   scanf("%d%d", &first, &second);
+ 
+   add = first + second;
+   subtract = first - second;
+   multiply = first * second;
+   
+
+   printf("Sum = %d\n", add);
+   printf("Difference = %d\n", subtract);
+   printf("Multiplication = %d\n", multiply);
+   
+ 
+   return 0;
+}
