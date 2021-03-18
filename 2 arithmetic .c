ff6f011f3d5b9ff479bb213c6b315415d9da3225
vishal561 <76278080+vishal561@users.noreply.github.com>
2021-03-18 12:27:59 -0700

    Update 2 arithmetic .c

--- 2 arithmetic .c
+++ 2 arithmetic .c
@@ -1,24 +1,26 @@
-#include <stdio.h>
-int main()
+//Name :Vishal Sharma
+//PURPOSE:ARITHMETIC OPERATIONS USING ADDITION, SUBTRACTION, MULTIPLICATION AND REMAINDER OPERATORS
+#include <stdio.h>//preprocessive directive to include standard input output header file
+int main() // main program starts
 {
-   int first, second, add, subtract, multiply ;
-   float divide ;
+   int first, second, add, subtract, multiply ;//variable declaration using integer data type
+   float divide ;//variable declration using float data type
    
  
-   printf("Enter two integers\n");
-   scanf("%d%d", &first, &second);
+   printf("Enter two integers\n");//printf function calling
+   scanf("%d%d", &first, &second);//asking user for input
  
-   add = first + second;
-   subtract = first - second;
-   multiply = first * second;
-   divide =   first / second;
+   add = first + second;//formula
+   subtract = first - second;//formula
+   multiply = first * second;//formula
+   divide =   first / second;//formula
    
 
-   printf("Sum = %d\n", add);
-   printf("Difference = %d\n", subtract);
-   printf("Multiplication = %d\n", multiply);
-   printf("Division = %f\n" , divide);
+   printf("Sum = %d\n", add);//printing sum of two numbers
+   printf("Difference = %d\n", subtract);//printing difference of two numbers
+   printf("Multiplication = %d\n", multiply);//print multiplication of two numbers
+   printf("Division = %f\n" , divide);//doubt
    
  
-   return 0;
+   return 0;//return statement
 }
