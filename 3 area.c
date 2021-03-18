9bdeef6beb652f0d188a52fe7a8899d252262a00
vishal561 <76278080+vishal561@users.noreply.github.com>
2021-03-18 12:32:58 -0700

    Update 3 area.c

--- 3 area.c
+++ 3 area.c
@@ -1,6 +1,6 @@
 //Creation date :16-03-2021
-//purpose:
-//To find the area and perimeter of Circle, Square and Rectangle
+//Name :Vishal sharma
+//purpose:To find the area and perimeter of Circle, Square and Rectangle
 //Area of Circle: (22/7)*r*r: radius
 //Perimeter of Circle: 2*(22/7)*r: radius
 //Area of Square: side*side: side
@@ -8,14 +8,14 @@
 //Area of Rectangle: Length * Breadth: Length, Breadth
 //Perimeter of Tectagnle: 2 * (Length + Breadth): Length, Breadth
 #include<stdio.h>
-int main(){
-	float raduisOfCircle, sideOfSquare, legthOfRectangle, breadthOfRectangle;
-	scanf("%f %f %f %f", &raduisOfCircle, &sideOfSquare, &legthOfRectangle, &breadthOfRectangle);
-	printf("Area of Circle: %f",((22*raduisOfCircle*raduisOfCircle)/7));
-	printf("\nPerimeter of Circle: %f",((2*22*raduisOfCircle)/7));
-	printf("\nArea of Square: %f",(sideOfSquare*sideOfSquare));
-	printf("\nPerimeter of Square: %f",(4*sideOfSquare));
-	printf("\nArea of Rectangle: %f",(legthOfRectangle*breadthOfRectangle));
-	printf("\nPerimeter of Rectangle: %f",(2*(legthOfRectangle+breadthOfRectangle)));
-	return 0;
+int main(){ //main programs starts
+	float raduisOfCircle, sideOfSquare, legthOfRectangle, breadthOfRectangle;//Variable declaration is taking place
+	scanf("%f %f %f %f", &raduisOfCircle, &sideOfSquare, &legthOfRectangle, &breadthOfRectangle);//expecting input form user
+	printf("Area of Circle: %f",((22*raduisOfCircle*raduisOfCircle)/7));//prints area of circle
+	printf("\nPerimeter of Circle: %f",((2*22*raduisOfCircle)/7));//prints perimeter of circle
+	printf("\nArea of Square: %f",(sideOfSquare*sideOfSquare));//prints arwa of square
+	printf("\nPerimeter of Square: %f",(4*sideOfSquare));//prints perimeter of square
+	printf("\nArea of Rectangle: %f",(legthOfRectangle*breadthOfRectangle));//prints area of rectangle
+	printf("\nPerimeter of Rectangle: %f",(2*(legthOfRectangle+breadthOfRectangle)));//prints perimeter of rectangle
+	return 0;//return statement
 }
