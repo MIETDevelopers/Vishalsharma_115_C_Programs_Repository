c3c2473a11112c1fbe1e0f2d30fc33234956ce0c
vishal561 <76278080+vishal561@users.noreply.github.com>
2021-03-18 12:19:22 -0700

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-int main() {
+#include <stdio.h>//preproccesive directive to include standard input output library
+int main() { //main function body starts
    // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+   printf("Hello, World!");//print f function calling
+   return 0;//return statement
 }
