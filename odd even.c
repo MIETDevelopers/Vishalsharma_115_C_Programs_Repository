e0145292e51265f6209321dda89991feb03b12b4
vishal561 <76278080+vishal561@users.noreply.github.com>
2021-03-18 12:38:37 -0700

    Update odd even.c

--- odd even.c
+++ odd even.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
+//Name Vishal sharma
+//Purpose :Find whether number is odd or even
+#include <stdio.h//preprocessive directive to include stdio header file
 int main() {
-    int num;
-    printf("Enter an integer: ");
-    scanf("%d", &num);
-    if(num % 2 == 0)
-        printf("%d is even.", num);
-    else
-        printf("%d is odd.", num);
+    int num;//variabe named num declared using int data type
+    printf("Enter an integer: ");//print f function
+    scanf("%d", &num);//reads value from user
+    if(num % 2 == 0)//IF-statement
+        printf("%d is even.", num);//If the condition  in If- statement is true then it will print this line
+    else//Else-statement
+        printf("%d is odd.", num);//If the condition in If-statement is false then it will print this line
 
-    return 0;
+    return 0;//return statement
 }
